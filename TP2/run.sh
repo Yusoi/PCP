4c4ca9b0e87fa8aa5acc26052e07edced020b5d9
Yusoi <a77230@alunos.uminho.pt>
2019-12-15 15:42:24 +0000

    TP2 complete, still missing Tcomp and Tcomm measurements

--- TP2/run.sh
+++ TP2/run.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 #
-#PBS -q mei
-#PBS -l nodes=2:ppn=
+#PBS -l nodes=2:ppn=32:r641
+#PBS -l walltime=1:00:00
+#PBS -o out/results.txt
+#PBS -e error.txt
+
+module load gcc/5.3.0
+module load gnu/openmpi_eth/1.8.4
+
+cd $PBS_O_WORKDIR
+
+array_num_machines=(2 4 6 8 10 12 14 16 20 24 28 32)
+
+for i in $array_num_machines
+do	
+	export NUM_MACHINES=$i
+	echo "Number of Machines: $NUM_MACHINES"
+	make clean
+	make NUM_MACHINES=$NUM_MACHINES
+	for i in {1..10}
+	do
+		mpirun -np $NUM_MACHINES --map-by ppr:1:core bin/tp2.o
+	done
+done
--- TP2/tp2.c
+++ TP2/tp2.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
+#include <time.h>
 
 #define N_MAX 1000
-#define N_MACHINES 24
 #define M_SIZE 1023
+//#define N_MACHINES 4
+//#define MEASUREMENTS
+//#define DEBUG
+
 
 int main(int argc, char *argv[])
 {
@@ -14,42 +18,87 @@ int main(int argc, char *argv[])
     int rank;
     MPI_Status status;
     int send[7] = {0};
-    int receive[3] = {0}
+    int receive[3] = {0};
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0)
-    {
-        int G1[M_SIZE][M_SIZE] = {{0}};
-
-        //Filling the lower line of the matrix with the highest heat
-        for (int i = 0; i < M_SIZE; i++)
-        {
-            G1[i][0] = 0xffffff; //Hexcode ffffff
+    int **G1 = (int**)malloc(sizeof(int*)*M_SIZE);
+    for(int i=0;i<M_SIZE;i++){
+	    G1[i] = (int*)malloc(sizeof(int)*M_SIZE);
+        for(int j = 0;j<M_SIZE;j++){
+            G1[i][j] = 0;
         }
     }
 
+    
+    //Filling the lower line of the matrix with the highest heat
+    for (int i = 0; i < M_SIZE; i++)
+    {
+        G1[i][0] = 0xffffff; //Hexcode ffffff
+    }
 
-    //Ping-pong Tcomm calculation
+    #ifdef DEBUG
+    printf("Rank: %d\n", rank);
+    #endif
+
     double start_time = MPI_Wtime();
-
-    int ex = 1;
     double end_time = 0;
 
-    if(rank == 0){
-        MPI_Send(ex, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    #ifdef MEASUREMENTS 
+    //TODO: Ping-pong Tcomm calculation
+    int ex = 1;
+
+    if (rank == 0)
+    {
+        #ifdef DEBUG
+        printf("First Send\n");
+        fflush(stdout);
+        #endif
+
+        MPI_Send(&ex, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+        #ifdef DEBUG
+        printf("First Send Sent\n");
+        fflush(stdout);
+        #endif
     }
-    if(rank == 1){
-        MPI_Receive(ex, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        double end_time = MPI_Wtime();
-        printf("Ts: %lf\nTcomm: %lf\n",end_time-start_time,(end_time-start_time)*4);
+    if (rank == 1)
+    {
+        #ifdef DEBUG
+        printf("First Receive\n");
+        fflush(stdout);
+        #endif
+
+        MPI_Recv(&ex, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+        #ifdef DEBUG
+        printf("First Receive Received\n");
+        fflush(stdout);
+        #endif
+
+        end_time = MPI_Wtime();                                //V Isto está errado
+        printf("Ts: %lf\nTcomm: %lf\n", end_time - start_time, (end_time - start_time) * N_MAX);
     }
 
+    //TODO: Tcomp calculation
+    if(rank == 0)
+    {
+        int pos = M_SIZE/2;
+        double s = MPI_Wtime();
+        int k = (G1[pos-1][pos]+
+                 G1[pos+1][pos]+
+                 G1[pos][pos-1]+
+                 G1[pos][pos+1]+
+                 G1[pos][pos])/5; 
+        double e = MPI_Wtime();
+        printf("Tcomp: %lf, k: %d\n",e-s,k);
+    }
 
+    #endif
 
     //Iterações sobre a difusão de calor
-    for (int it = 0; it < N_MAX; i++)
+    for (int it = 0; it < N_MAX; it++)
     {
         if (rank == 0)
         {
@@ -65,20 +114,54 @@ int main(int argc, char *argv[])
                     send[5] = i;
                     send[6] = j;
 
-                    MPI_Send(send, 7, MPI_INT, ((i + j) % (N_MACHINES - 1)) + 1, 0, MPI_COMM_WORLD);
+                    #ifdef DEBUG
+                    printf("Second Send Rank: %d\n",((i + j) % (N_MACHINES - 1)) + 1);
+                    fflush(stdout);
+                    #endif
+
+                    MPI_Send(&send, 7, MPI_INT, ((i + j) % (N_MACHINES - 1)) + 1, 0, MPI_COMM_WORLD);
+
+                    #ifdef DEBUG
+                    printf("Second Send Sent\n");
+                    fflush(stdout);
+                    #endif
                 }
             }
 
             //Tells all machines the iterations have ended
-            buffer[0] = -1;
-            MPI_Bcast(send, 7, MPI_INT, k, 0, MPI_COMM_WORLD);
+            send[0] = -1;
+
+            #ifdef DEBUG
+            printf("First Broadcast\n");
+            fflush(stdout);
+            #endif
+
+            for(int i = 1; i <= N_MACHINES - 1 ; i++){
+                MPI_Send(&send, 7, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+
+            #ifdef DEBUG
+            printf("First Broadcast Sent\n");
+            fflush(stdout);
+            #endif
 
             //Copiar G2 para G1
             for (int i = 1; i < M_SIZE - 1; i++)
             {
                 for (int j = 1; j < M_SIZE - 1; j++)
                 {
-                    MPI_Receive(receive, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+                    #ifdef DEBUG
+                    printf("Second Receive\n");
+                    fflush(stdout);
+                    #endif
+
+                    MPI_Recv(&receive, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+
+                    #ifdef DEBUG
+                    printf("Second Receive Received\n");
+                    fflush(stdout);
+                    #endif
+
                     G1[receive[1]][receive[2]] = receive[0];
                 }
             }
@@ -88,21 +171,54 @@ int main(int argc, char *argv[])
             int done = 0;
             while (!done)
             {
-                MPI_Receive(send, 7, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-                if (buffer[0] != -1)
+                #ifdef DEBUG
+                printf("Third Receive\n");
+                fflush(stdout);
+                #endif
+
+                MPI_Recv(&send, 7, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+                #ifdef DEBUG
+                printf("Third Receive Received\n");
+                fflush(stdout);
+                printf("%d %d %d %d %d %d %d\n", send[0], send[1], send[2], send[3], send[4], send[5], send[6]);
+                #endif
+
+                if (send[0] != -1)
                 {
                     receive[0] = (send[0] + send[1] + send[2] + send[3] + send[4]) / 5;
                     receive[1] = send[5];
                     receive[2] = send[6];
-                    MPI_Send(receive, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+
+                    #ifdef DEBUG
+                    printf("Third Send\n");
+                    fflush(stdout);
+                    #endif
+
+                    MPI_Send(&receive, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+                    #ifdef DEBUG
+                    printf("Third Send Sent\n");
+                    fflush(stdout);
+                    #endif
                 }
                 else
                 {
+                    #ifdef DEBUG
+                    printf("Done\n");
+                    fflush(stdout);
+                    #endif
+
                     done = 1;
                 }
             }
         }
     }
+
+    end_time = MPI_Wtime();
+
+    if(rank == 0)
+        printf("Total time: %lf seconds\n",end_time-start_time);
 
     MPI_Finalize();
 
@@ -116,4 +232,6 @@ int main(int argc, char *argv[])
 
         fprintf(file, "\n");
     }
+
+    return 0;
 }
