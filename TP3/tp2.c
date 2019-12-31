4c185132afa2e684792b671ec1b45997e458f20b
Yusoi <a77230@alunos.uminho.pt>
2019-12-31 15:29:57 +0000

    Almost completed tp2.c

--- TP3/tp2.c
+++ TP3/tp2.c
@@ -6,45 +6,27 @@
 #define N_MAX 1000
 #define N_MACHINES 4
 #define MAT_SIZE 1024
-#define M_SIZE MAT_SIZE + 2
+#define M_SIZE (MAT_SIZE+2)
 
 int main(int argc, char *argv[])
 {
     FILE *file = fopen("result.txt", "w+");
-
-    /*
-    int **G1;
-    G1 = (int **)malloc(sizeof(int *) * M_SIZE);
-
-    for (int i = 0; i < M_SIZE; i++)
-    {
-        G1[i] = (int *)malloc(sizeof(int) * M_SIZE);
-
-        for (int j = 0; j < M_SIZE; j++)
-        {
-            G1[i][j] = 0;
-        }
-    }
-
-    //Filling the lower line of the matrix with the highest heat
-    for (int i = 0; i < M_SIZE; i++)
-    {
-        G1[i][0] = 0xffffff; //Hexcode ffffff
-    }
-    */
-
-    //------------------------------------------------------------------------------------------
+    
     int rank;
-    MPI_Status status;
     int i_division = MAT_SIZE / N_MACHINES;
+    int MACH_MAT_SIZE = i_division * M_SIZE;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     double start_time = MPI_Wtime();
     int left_send_buffer[M_SIZE], right_send_buffer[M_SIZE], left_recv_buffer[M_SIZE], right_recv_buffer[M_SIZE];
+    int *final_send_buffer = (int *)malloc(sizeof(int) * M_SIZE * i_division);
+    int *final_result_buffer = (int *)malloc(sizeof(int) * M_SIZE * MAT_SIZE);
     MPI_Request left_send_request, right_send_request, left_recv_request, right_recv_request;
+    printf("MACHINE MATRIX SIZE: %d, RESULT MATRIX SIZE: %d\n",MACH_MAT_SIZE,M_SIZE*MAT_SIZE);
 
     int **G1 = (int **)malloc(sizeof(int *) * i_division);
+    int **G2 = (int **)malloc(sizeof(int *) * i_division);
     for (int i = 0; i < i_division; i++)
     {
         G1[i] = (int *)malloc(sizeof(int) * M_SIZE);
@@ -61,8 +43,8 @@ int main(int argc, char *argv[])
 
     for (int j = 0; j < M_SIZE; j++)
     {
-        left_recv_buffer[M_SIZE] = 0;
-        right_recv_buffer[M_SIZE] = 0;
+        left_recv_buffer[j] = 0;
+        right_recv_buffer[j] = 0;
     }
 
     //Iterações sobre a difusão de calor
@@ -78,22 +60,37 @@ int main(int argc, char *argv[])
         }
 
         //Waits to receive the left buffer
-        MPI_Wait(&left_recv_request, MPI_STATUS_IGNORE);
+        if (it != 0 && rank != 0)
+        {
+            MPI_Wait(&left_recv_request, MPI_STATUS_IGNORE);
+        }
         for (int j = 1; j < M_SIZE - 1; j++)
         {
             G2[0][j] = (left_recv_buffer[j] + G1[1][j] + G1[0][j - 1] + G1[0][j + 1] + G1[0][j]) / 5;
         }
 
         //Waits to receive the right buffer
-        MPI_Wait(&right_recv_request, MPI_STATUS_IGNORE);
+        if (it != 0 && rank != N_MACHINES - 1)
+        {
+            MPI_Wait(&right_recv_request, MPI_STATUS_IGNORE);
+        }
         for (int j = 1; j < M_SIZE - 1; j++)
         {
             G2[i_division - 1][j] = (G1[i_division - 2][j] + right_recv_buffer[j] + G1[i_division - 1][j - 1] + G1[i_division - 1][j + 1] + G1[i_division - 1][j]) / 5;
         }
 
         //Guarantees the buffers have been sent
-        MPI_Wait(&left_send_request, MPI_STATUS_IGNORE);
-        MPI_Wait(&right_send_request, MPI_STATUS_IGNORE);
+        if (it != 0)
+        {
+            if (rank != 0)
+            {
+                MPI_Wait(&left_send_request, MPI_STATUS_IGNORE);
+            }
+            if (rank != N_MACHINES - 1)
+            {
+                MPI_Wait(&right_send_request, MPI_STATUS_IGNORE);
+            }
+        }
         //Copies the column to the buffer
         for (int j = 0; j < M_SIZE; j++)
         {
@@ -106,13 +103,13 @@ int main(int argc, char *argv[])
         //Sends and receives assynchonously the buffers
         if (rank != 0)
         {
-            left_send_request = MPI_Isend(&left_send_buffer);
-            left_recv_request = MPI_Irecv(&left_recv_buffer);
+            MPI_Isend(&left_send_buffer, M_SIZE, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &left_send_request);
+            MPI_Irecv(&left_recv_buffer, M_SIZE, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &left_recv_request);
         }
         if (rank != N_MACHINES - 1)
         {
-            right_send_request = MPI_Isend(&right_send_buffer);
-            right_recv_request = MPI_Irecv(&right_recv_buffer);
+            MPI_Isend(&right_send_buffer, M_SIZE, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &right_send_request);
+            MPI_Irecv(&right_recv_buffer, M_SIZE, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &right_recv_request);
         }
 
         //Copies from G2 to G1
@@ -125,14 +122,53 @@ int main(int argc, char *argv[])
         }
     }
 
-    //TODO: rank 0 gathers results from all other ranks
+    for (int i = 0; i < i_division; i++)
+    {
+        for (int j = 0; j < M_SIZE; j++)
+        {
+            final_send_buffer[i * M_SIZE + j] = G1[i][j];
+        }
+    }
+
+    printf("PREGATHER rank: %d\n", rank);
+    fflush(stdout);
+
+    //TODO resolver problema com o gather
+
+    //Rank 0 gathers results from all other ranks
+    //int MPI_Gather(void* sendbuf, int sendcount, MPI_Datatype sendtype, void* recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm)
+    MPI_Gather(&final_send_buffer, MACH_MAT_SIZE, MPI_INT, &final_result_buffer, MACH_MAT_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("POSTGATHER rank: %d\n", rank);
+    fflush(stdout);
 
     double end_time = MPI_Wtime();
 
-    MPI_Finalize();
-
     if (rank == 0)
     {
+        //Creates the final matrix to output the result
+        int **FINAL_MAT = (int **)malloc(sizeof(int *) * i_division);
+        for (int i = 0; i < i_division; i++)
+        {
+            FINAL_MAT[i] = (int *)malloc(sizeof(int) * M_SIZE);
+        }
+        for (int j = 0; j < M_SIZE; j++)
+        {
+            FINAL_MAT[0][j] = 0;
+            FINAL_MAT[M_SIZE - 1][j] = 0;
+        }
+        FINAL_MAT[0][0] = 0xffffff;
+        FINAL_MAT[M_SIZE - 1][0] = 0xffffff;
+
+        //Copies the result from the receive buffer to the result matrix
+        for (int i = 0; i < MAT_SIZE; i++)
+        {
+            for (int j = 0; j < M_SIZE; j++)
+            {
+                FINAL_MAT[i + 1][j] = final_result_buffer[i * M_SIZE + j];
+            }
+        }
+
         printf("Total time: %lf seconds\n", end_time - start_time);
 
         //Prints results to a file
@@ -144,4 +180,6 @@ int main(int argc, char *argv[])
 
         fprintf(file, "\n");
     }
+
+    MPI_Finalize();
 }
